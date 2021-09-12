872a5da758c2dd936184a20048161b1315f98bd0
Alan Donahue <alan.justin.donahue@gmail.com>
2021-09-12 17:17:49 -0500

    exercise 2.1 assignment

---
+++ Exercise 2.1/Donahue,Alan Exercise 2.1.py
@@ -0,0 +1,29 @@
+# part 1
+print("Hello, World! I wonder why that is always the default coding text to start with")
+
+# part 2
+print(2+2)
+
+# part 3
+print(10-5)
+
+# part 4
+print(5*4)
+
+# part 5
+print(10/2)
+
+# part 6
+print("First" + " Second")
+
+# part 7
+myList = [1, 2, 3, 4, 5]
+print(myList)
+
+# part 8
+myList.append(6)
+print(myList)
+
+# part 9
+myTuple = ("James", "Billy", "Lisa", "Andy")
+print(myTuple)
